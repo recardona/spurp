ce7320800378b1c0cd68779a15e2616a8b2ca393
R. Michael Young <young@cs.utah.edu>
2018-02-09 13:31:06 -0700

    missed these last time

---
+++ Assets/Scripts/ParallaxManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxManager : MonoBehaviour {
+
+	public GameObject focus;
+
+	public int xrate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		 float diff = transform.x - focus.GetComponent<Transform>().position.x;
+
+	}
+}
